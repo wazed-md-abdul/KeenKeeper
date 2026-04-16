d753d8e9b4f0dd0d31344190ff6a761a14256ea8
wazed-md-abdul <wazedjiad@gmail.com>
2026-04-16 18:55:22 +0800

    loading period decreased

--- src/hooks/useHooks.jsx
+++ src/hooks/useHooks.jsx
@@ -11,7 +11,7 @@ const useHooks = () => {
             setTimeout(() => {
                 setFriends(data);
                 setLoading(false);
-            }, 2000);
+            }, 500);
         }
 
 
